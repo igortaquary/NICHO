a6b811140be31262d3b923c7d749565561ca27d7
Guilherme Rodrigues <guilodron7@gmail.com>
2021-03-04 15:10:00 -0300

    Fix status bar

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MainPages from './main';
 import AuthPages from './auth';
+import { StatusBar } from 'react-native';
 
-const userLogged = true;
+const userLogged = false;
 
 const Routes = () => {
 
     return(
         <NavigationContainer>
+            <StatusBar backgroundColor='transparent' barStyle='dark-content' />
             { userLogged ? <MainPages /> : <AuthPages /> }
         </NavigationContainer>
     ) 
